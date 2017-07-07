cdcfa204bd969877c6c8f1809bf323f066e54b7d
michael.malone@aofl.com <michael.malone@aofl.com>
2017-07-06 18:51:59 -0700

    Made major updates to the script to output readable messages.

--- 0.1/scripts/gdf_validate.sh
+++ 0.1/scripts/gdf_validate.sh
@@ -4,31 +4,26 @@ CID=$1
 #GDFV=$3
 HOME=~
 ASSETDIR=$2
-TESTLOCATION="Documents/Development/gdf_validation/0.1"
-VALIDATION_PASSED="false"
+TESTLOCATION="Documents/Development/gdf_validator_malone/0.1"
+PASS_FAIL="FAILED"
+exception=1
+Red='\033[0;31m'
+Green='\033[0;32m'
+Blue='\033[0;34m'
+normal='\e[0m'
 
 #run this script before this line: if ! [[ -d "${HOME}/gdf_packets/" && -x "${HOME}/gdf_packets/" ]]; then of the build_game script in /usr/local/bin 
 function init_validation() {
 	default_path=default/path/to/source.txt
-	#echo "List the file path" 
-	#read file_path
-	#if [ "$file_path" ];
-	#then 
-		#read file_path
-		#echo "The file path is: $file_path" 
-	#else
-		#echo "You did not specify a path.  Using default path of $default_path"
- 		#echo "The file path is: $file_path" 
-	#fi
 	validate_affirmation
 	validate_background
 	validate_gameplay
+	validate_instructions
 }
 
 function validate_affirmation() {
 	#Read the text file
-	echo "The CID $CID"
-	echo "The HOME $HOME"
+	echo "Validating affirmations...."
   fileName="$HOME/$TESTLOCATION/tmp/games/$CID/assets/SOURCE/animations/affirmation/animation.js"
   referenced_obj_line=""
   targetLine=""
@@ -52,15 +47,13 @@ function validate_affirmation() {
   #Find the line that contains the text matching the 'frame_aff and required_object' variables above
   while IFS='' read -r line
   do
-    val="$line"
     #Find occurance of required_object
-    if [[ "$val" =~ "$required_object" ]];
+    if [[ "$line" =~ "$required_object" ]];
     then
-    	echo "Found the required Object, called $required_object."
-    	referenced_obj_line="$required_object"
+    	referenced_obj_line="$line"
     fi
     #Find occurance of frame_aff
-    if [[ "$val" =~ "$frame_aff" ]];
+    if [[ "$line" =~ "$frame_aff" ]];
       then 
         targetLine="$line"
     fi
@@ -75,43 +68,41 @@ function validate_affirmation() {
   if [[ "$referenced_obj_line" == "" ]];
   then
   	echo "No occurances of $required_object* were found in the affiration/animation.js file."
+  else
+  	echo "Found the required $required_object object"
   	#exit with error code 101 which can be mapped to specified string not found in animation.js: $frame_aff
   fi
-  
-  #echo "The Target Line: $targetLine"TODO remove this
-  
+    
   #Iterate through the long string to match the occurance of the affirmation string
   IFS=","
   for item in $targetLine; do
   #for item in ${validationArray[@]}; do
   	if [[ "$item" =~ "$frame_aff" ]];
   	then
-  		#echo "The item: $item"
-  		#echo "Found Affirmation:  $item" | sed 's/{//' | sed 's/}//' | sed 's/);//'
-
-  		#echo ": $item" | sed 's/{//' | sed 's/}//' | sed 's/);//' | cut -d ':' -f 2
-
   		#Strip the characters after the ':' from the string that matches the frame_aff variable
   		cleanItem=$(echo ':'$item | sed 's/{//' | sed 's/}//' | sed 's/);//' | cut -d ':' -f 2) 
-  		
-  		echo "Clean Item $cleanItem"
-  		#echo "Found Affirmation entry:  $item"
-  		
+  		  		
   		if [[ "${validationArray[occurance]}" == "$cleanItem" ]];
   		then
   			echo "Found affirmation : ${validationArray[occurance]}"
   			unset validationArray[occurance]
   		fi
-  		
-  		echo "The validationArray: ${validationArray[occurance]}"
   		occurance=`expr $occurance + 1`
   	fi
   done
+  
+  if [[ "${validationArray[@]}" == "" && "$referenced_obj_line" != "" ]];
+  then 
+  	PASS_FAIL="PASSED"
+  else
+  	PASS_FAIL="FAILED"
+  fi
   #Run acceptance function to raise an error if needed.  Create gdf_error.log file to print the error code and the the 
-  echo $occurance
+  echo -e "VALIDATION FOR $CID/assets/SOURCE/animations/affirmation/animation.js HAS $PASS_FAIL\n"
 }
 
 function validate_background() {
+	echo "Validating background...."
 	fileName="$HOME/$TESTLOCATION/tmp/games/$CID/assets/SOURCE/animations/background/animation.js"
 	required_obj="lib._animation_Background"
 	required_line_obj=""
@@ -120,136 +111,203 @@ function validate_background() {
 	do 
 		if [[ "$line" =~ "$required_obj" ]];
 		then
-			echo "found the line with the value $line"
+			echo "Found the $required_obj object"
 			required_line_obj="$line"
 		fi
 	done < "$fileName" 
 		
 	if [[ "$required_line_obj" == "" ]];
 	then
-		echo "No occurances of $required_obj we found in background/animation.js"
+		PASS_FAIL="FAILED"
+		echo "No occurances of $required_obj we found in $CID/assets/SOURCE/animations/background/animation.js"
+	else
+		PASS_FAIL="PASSED"
 		#exit 102 Map to error message
 	fi
+	#echo "No occurances of $required_obj we found in background/animation.js.  "
+	echo -e "VALIDATION FOR $CID/assets/SOURCE/animations/background/animation.js HAS $PASS_FAIL\n"
 }
 
 
 function validate_gameplay() {
-	fileName="$HOME/$TESTLOCATION/tmp/games/$CID/assets/SOURCE/animations/gameplay/animation.js"
-	required_lib_obj_gameplay="lib._animation_Gameplay"
-	required_lib_obj_CountdownAnimationIntro="lib._animation_CountdownAnimationIntro"
-	required_lib_obj_CountdownAnimationPayoff="lib._animation_CountdownAnimationPayoff"
-	required_lib_obj_CountdownAnimationTimer="lib._animation_CountdownAnimationTimer"
-	required_lib_obj_CountDownBar="lib._animation_CountDownBar"
-	required_lib_obj_CountdownIdle="lib._animation_CountdownIdle"
-	required_lib_obj_CountdownIdleLoop="_frame_CountdownIdleLoop"
-	required_lib_obj_CurrentRoundDisplay="lib._animation_CurrentRoundDisplay"
-	required_lib_obj_ScoreBar="lib._animation_ScoreBar"
-	frame_obj_Countdown="_frame_Countdown"
-	frame_obj_CountdownComplete="_frame_CountdownComplete"
-	frame_obj_frame_CountdownIdleLoop="_frame_CountdownIdleLoop"
-	frame_obj_CountdownLoopStart="_frame_CountdownLoopStart"
-	frame_obj_CountdownLoopEnd="_frame_CountdownLoopEnd"
-	required_line_obj=""
+	echo "Validating gameplay...."
+	i=0;
 	
+	fileName="$HOME/$TESTLOCATION/tmp/games/$CID/assets/SOURCE/animations/gameplay/animation.js"
+	required_line_obj=""
 	validationArray=(
-	$required_lib_obj_gameplay 
-	$required_lib_obj_CountdownAnimationIntro 
-	$required_lib_obj_CountdownAnimationPayoff
-	$required_lib_obj_CountdownAnimationTimer
-	$required_lib_obj_CountDownBar
-	$required_lib_obj_CountdownIdle
-	$required_lib_obj_CountdownIdleLoop
-	$required_lib_obj_CurrentRoundDisplay
-	$required_lib_obj_ScoreBar
-	$frame_obj_Countdown
-	$frame_obj_CountdownComplete
-	$frame_obj_frame_CountdownIdleLoop
-	$frame_obj_CountdownLoopStart
-	$frame_obj_CountdownLoopEnd
+	"lib._animation_Gameplay"
+	"lib._animation_CountdownAnimationIntro"
+	"lib._animation_CountdownAnimationPayoff"
+	"lib._animation_CountdownAnimationTimer"
+	"lib._animation_CountDownBar"
+	"lib._animation_CountdownIdle"
+	"_frame_CountdownIdleLoop"
+	"lib._animation_CurrentRoundDisplay"
+	"lib._animation_ScoreBar"
+	"_frame_Countdown"
+	"_frame_CountdownComplete"
+	"_frame_CountdownIdleLoop"
+	"_frame_CountdownLoopStart"
+	"_frame_CountdownLoopEnd"
 	)
+	
 	while IFS='' read -r line
 	do
-		if [[ "$line" =~ "$required_lib_obj_gameplay " ]];
-		then
-			echo "Found the $required_lib_obj_gameplay object"
-			unset validationArray[0]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CountdownAnimationIntro" ]];
-		then
-			echo "Found the $required_lib_obj_CountdownAnimationIntro object"
-			unset validationArray[1]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CountdownAnimationPayoff" ]];
-		then
-			echo "Found the $required_lib_obj_CountdownAnimationPayoff object"
-			unset validationArray[2]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CountdownAnimationTimer" ]];
-		then
-			echo "Found the $required_lib_obj_CountdownAnimationTimer object"
-			unset validationArray[3]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CountDownBar" ]];
-		then
-			echo "Found the $required_lib_obj_CountDownBar object"
-			unset validationArray[4]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CountdownIdle" ]];
-		then
-			echo "Found the $required_lib_obj_CountdownIdle object"
-			unset validationArray[5]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CountdownIdleLoop" ]];
-		then
-			echo "Found the $required_lib_obj_CountdownIdleLoop object"
-			unset validationArray[6]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_CurrentRoundDisplay" ]];
-		then
-			echo "Found the $required_lib_obj_CurrentRoundDisplay object"
-			unset validationArray[7]
-		fi
-		if [[ "$line" =~ "$required_lib_obj_ScoreBar" ]];
-		then
-			echo "Found the $required_lib_obj_ScoreBar object"
-			unset validationArray[8]
-		fi
-		if [[ "$line" =~ "$frame_obj_Countdown" ]];
-		then
-			echo "Found the $frame_obj_Countdown object"
-			unset validationArray[9]
-		fi
-		if [[ "$line" =~ "$frame_obj_CountdownComplete" ]];
-		then
-			echo "Found the $frame_obj_CountdownComplete object"
-			unset validationArray[10]
-		fi
-		if [[ "$line" =~ "$frame_obj_frame_CountdownIdleLoop" ]];
-		then
-			echo "Found the $frame_obj_frame_CountdownIdleLoop object"
-			unset validationArray[11]
-		fi
-		if [[ "$line" =~ "$frame_obj_CountdownLoopStart" ]];
-		then
-			echo "Found the $frame_obj_CountdownLoopStart object"
-			unset validationArray[12]
-		fi		
-		if [[ "$line" =~ "$frame_obj_CountdownLoopEnd" ]];
-		then
-			echo "Found the $frame_obj_CountdownLoopEnd object"
-			unset validationArray[13]
-		fi
+		#Scan each line for these reqirements and remove entry from the array if found
+		if [[ "$line" =~ "${validationArray[0]}" ]];
+    then
+      echo "Found the ${validationArray[0]} object"
+      unset validationArray[0]
+    fi
+    if [[ "$line" =~ "${validationArray[1]}" ]];
+    then
+      echo "Found the ${validationArray[1]} object"
+      unset validationArray[1]
+    fi
+    if [[ "$line" =~ "${validationArray[2]}" ]];
+    then
+      echo "Found the ${validationArray[2]} object"
+      unset validationArray[2]
+    fi
+    if [[ "$line" =~ "${validationArray[3]}" ]];
+    then
+      echo "Found the ${validationArray[3]} object"
+      unset validationArray[3]
+    fi
+    if [[ "$line" =~ "${validationArray[4]}" ]];
+    then
+      echo "Found the ${validationArray[4]} object"
+      unset validationArray[4]
+    fi
+    if [[ "$line" =~ "${validationArray[5]}" ]];
+    then
+      echo "Found the ${validationArray[5]} object"
+      unset validationArray[5]
+    fi
+    if [[ "$line" =~ "${validationArray[6]}" ]];
+    then
+      echo "Found the ${validationArray[6]} object"
+      unset validationArray[6]
+    fi
+    if [[ "$line" =~ "${validationArray[7]}" ]];
+    then
+      echo "Found the ${validationArray[7]} object"
+      unset validationArray[7]
+    fi
+    if [[ "$line" =~ "${validationArray[8]}" ]];
+    then
+      echo "Found the ${validationArray[8]} object"
+      unset validationArray[8]
+    fi
+    if [[ "$line" =~ "${validationArray[9]}" ]];
+    then
+      echo "Found the ${validationArray[9]} object"
+      unset validationArray[9]
+    fi
+    if [[ "$line" =~ "${validationArray[10]}" ]];
+    then
+      echo "Found the ${validationArray[10]} object"
+      unset validationArray[10]
+    fi
+    if [[ "$line" =~ "${validationArray[11]}" ]];
+    then
+      echo "Found the ${validationArray[11]} object"
+      unset validationArray[11]
+    fi
+    if [[ "$line" =~ "${validationArray[12]}" ]];
+    then
+      echo "Found the ${validationArray[12]} object"
+      unset validationArray[12]
+    fi    
+    if [[ "$line" =~ "${validationArray[13]}" ]];
+    then
+      echo "Found the ${validationArray[13]} object"
+      unset validationArray[13]
+    fi
+		
 	done < "$fileName"
 	
 	#echo "The validationArray ${validationArray[@]}"
 	if [[ "${validationArray[@]}" == "" ]];
 	then
-		VALIDATION_PASSED=true
+		PASS_FAIL="PASSED"
 	else
-		VALIDATION_PASSED=false
+		PASS_FAIL="FAILED"
 		echo "The missing values from $CID/assets/SOURCE/animations/gameplay/animation.js are: ${validationArray[@]}"
 	fi
-	echo "VALIDATION_PASSED:  $VALIDATION_PASSED"
+	echo -e "VALIDATION FOR $CID/assets/SOURCE/animations/gameplay/animation.js has $PASS_FAIL\n"
+}
+
+function validate_instructions() {
+	echo "Validating instructions...."
+	i=0
+	j=0
+	fileName="$HOME/$TESTLOCATION/tmp/games/$CID/assets/SOURCE/animations/instructions/animation.js"
+	required_object_instructions="lib._animation_Instructions"
+	required_object_playbutton="lib._animation_PlayButton"
+	target_line=""
+	target_item=""
+	
+	required_object_array=(
+	$required_object_instructions
+	$required_object_playbutton
+	)
+	
+	validationArray=(
+		"_frame_Intro"
+		"_frame_Loop"
+		"_frame_Outro"
+	)
+	
+	while IFS='' read -r line
+	do
+		#Locate the items in the required_object_array
+		if [[ "$line" =~ "${required_object_array[i]}" ]];
+		then
+			echo "Found the ${required_object_array[i]}"
+			unset required_object_array[i]
+			i=`expr $i + 1`
+		fi
+		
+		#Find the target line which contains the _frame_* text
+		if [[ "$line" =~ "${validationArray[0]}" ]];
+		then
+			echo "Found the instructions line:  The Array: ${validationArray[0]}"
+			target_line="$line"
+			#j=`expr $j + 1`
+		fi
+	done < "$fileName" 
+	
+	#Isolate the _frame_* items from the animation.js script
+	IFS="{}"
+	for item in $target_line; do
+		if [[ "$item" =~ "${validationArray[0]}" ]];
+		then
+			echo "Matches: $item"
+			target_item="$item"
+		fi
+	done
+	
+	#iterate through item line  to find matching validation array items
+	IFS=","
+	for frameItem in $target_item; do
+		if [[ "$frameItem" =~ "${validationArray[$j]}" ]];
+		then
+			echo "Found required items: $frameItem"
+			unset validationArray[j]
+		fi
+		j=`expr $j + 1`
+	done
+
+if [[ "${required_object_array[@]}" == "" && ${validationArray[@]} == "" ]];
+	then
+		PASS_FAIL="PASSED"
+	else
+		PASS_FAIL="FAILED"
+		echo "These are the missing elements from $CID/assets/SOURCE/animations/instructions/animation.js: ${required_object_array[@]} ${validationArray[@]}"
+	fi
+	echo  -e "VALIDATION FOR $CID/assets/SOURCE/animations/instructions/animation.js has $PASS_FAIL\n"
 }
 #accept the CID and the asset directory if needed....maybe not needed for now
 init_validation $CID $ASSETDIR
